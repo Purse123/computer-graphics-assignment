33c4b2cbd9c3cb14f1272c983df35c7a314674a5
Pierce Neupane <sahasneupane85@gmail.com>
2025-03-22 14:00:57 +0545

    program completed

--- OPENGL/11-opengl_line.c
+++ OPENGL/11-opengl_line.c
@@ -1,23 +1,32 @@
-#include <GLFW/glfw3.h>
 #include <GL/gl.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
 
 int main() {
   if (!glfwInit()) {
     return -1;
   }
 
-  GLFWwindow* window = glfwCreateWindow(600, 400, "Tero bau", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(800, 600, "Pierce - st.line", NULL, NULL);
   if (!window) {
     glfwTerminate();
     return -1;
   }
+
   glfwMakeContextCurrent(window);
 
   while (!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_LINES); // GL_LINES for line
+        glColor3f(1.0f, 1.0f, 1.0f);
+        glVertex2f(-0.5f, 0.5f);
+        glVertex2f(0.5f, 0.5f);
+    glEnd();
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+  glfwDestroyWindow(window);
   glfwTerminate();
   return 0;
 }
---
+++ OPENGL/12-traingle.c
@@ -0,0 +1,34 @@
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+int main() {
+  if (!glfwInit()) {
+    return -1;
+  }
+
+  GLFWwindow* window = glfwCreateWindow(800, 600, "Pierce - st.line", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES); // GL_TRIANGELS
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f(0, 0.5f);
+    glVertex2f(0.5f, -0.5f);
+    glVertex2f(-0.5f, 0.5f);
+    glEnd();
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
+}
